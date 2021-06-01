beadd46b84a8310c82851004562c74a99f0532aa
ihch <nemu0sou@gmail.com>
2021-06-02 00:20:57 +0900

    chore: wrote a example component

---
+++ components/example.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import styles from './example.module.scss';
+
+type ExampleProps = {
+  title: string;
+  description: string;
+};
+
+const ExamplePresentation: React.VFC<ExampleProps> = ({ title, description }) => {
+  return (
+    <div className={styles['example-component']}>
+      <h2 className={styles['title']}>{title}</h2>
+      <p className={styles['description']}>{description}</p>
+    </div>
+  );
+};
+
+const ExampleContainer: React.FC<ExampleProps> = (props) => {
+  return <ExamplePresentation {...props} />;
+};
+
+export default ExampleContainer;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,3 +1,11 @@
+import React from 'react';
+import ExampleContainer from '~/example';
+
 export default function Home() {
-  return <div>Hello Daizu Online Judge!</div>;
+  return (
+    <div>
+      Hello Daizu Online Judge!
+      <ExampleContainer title='タイトル' description='説明文' />
+    </div>
+  );
 }
