257f6008c4e6d70095b11824c8c2f6bc3d784f1c
ihch <nemu0sou@gmail.com>
2021-06-02 21:47:43 +0900

    feat: add css config

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,7 +1,17 @@
 import '../styles/globals.css';
+import './_app.scss';
 import type { AppProps } from 'next/app';
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+  return (
+    <div>
+      <Head>
+        <link rel='preconnect' href='https://fonts.gstatic.com' />
+        <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' rel='stylesheet' />
+      </Head>
+      <Component {...pageProps} />
+    </div>
+  );
 }
 export default MyApp;
