50d8a97ab6c48aeb917b75cb1747a6ccb2345d5c
ihch <nemu0sou@gmail.com>
2021-06-02 22:51:10 +0900

    fix: ヘッダーコンポーネントをページ共通定義に加えた

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,6 +2,8 @@ import '../styles/globals.css';
 import './_app.scss';
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import React from 'react';
+import HeaderContainer from '~/common/Header';
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
@@ -10,6 +12,7 @@ function MyApp({ Component, pageProps }: AppProps) {
         <link rel='preconnect' href='https://fonts.gstatic.com' />
         <link href='https://fonts.googleapis.com/css2?family=Roboto:wght@400;700&display=swap' rel='stylesheet' />
       </Head>
+      <HeaderContainer />
       <Component {...pageProps} />
     </div>
   );
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import HeaderContainer from '~/common/Header';
 import ExampleContainer from '~/example';
 
 export default function Home() {
   return (
     <div>
-      <HeaderContainer />
       Hello Daizu Online Judge!
       <ExampleContainer title='タイトル' description='説明文' />
     </div>
